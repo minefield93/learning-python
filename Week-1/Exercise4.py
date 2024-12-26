91e1c103d15dda35671a20bcdb8d47432b1f5c83
Saurabh Bashirabadkar <smbashirabadkar@gmail.com>
2024-12-25 19:13:18 -0700

    Adding Exercise4.py

---
+++ Week-1/Exercise4.py
@@ -0,0 +1,10 @@
+
+# Assigning string to var line
+line = "Processor board ID FAL127197A"
+
+# Performing membership check
+mem_check = "Processor board ID" in line
+print()
+print(f"The membership check is {mem_check}")
+print()
+ 

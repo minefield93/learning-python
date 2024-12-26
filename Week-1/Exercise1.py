da9b45fdd09d6b1b4eda9f5d9b328c0ef3e978ef
Saurabh Bashirabadkar <saurabhbashirabadkar@Saurabhs-MacBook-Pro.local>
2024-12-25 18:30:56 -0700

    Adding Exercise1.py

---
+++ Week-1/Exercise1.py
@@ -0,0 +1,12 @@
+sf_gw1 = "172.31.255.1/24"
+sf_gw2 = input("Please input 'sf_gw2' IP address: ")
+
+header1 = "sf_gw1"
+header2 = "sf_gw2"
+header_line = "-" * 20
+
+print()
+print(f"{header1:^20} {header2:^20}")
+print(f"{header_line:^20} {header_line:^20}")
+print(f"{sf_gw1:^20} {sf_gw2:^20}")
+print()

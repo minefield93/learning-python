03fcfaa3e1829890fa949896dc5a0eeea6c4d16e
Saurabh Bashirabadkar <smbashirabadkar@gmail.com>
2024-12-25 18:59:18 -0700

    Adding comments

--- Week-1/Exercise1.py
+++ Week-1/Exercise1.py
@@ -1,10 +1,13 @@
+# Assigning variables
 sf_gw1 = "172.31.255.1/24"
 sf_gw2 = input("Please input 'sf_gw2' IP address: ")
 
+# Creating new variables to use for creating columns
 header1 = "sf_gw1"
 header2 = "sf_gw2"
 header_line = "-" * 20
 
+# Printing using f-string centering method
 print()
 print(f"{header1:^20} {header2:^20}")
 print(f"{header_line:^20} {header_line:^20}")
--- Week-1/Exercise2.py
+++ Week-1/Exercise2.py
@@ -1,14 +1,19 @@
+
+# Asking for dc name
 print()
 dc_name = input("Enter the data center location: ")
 
+# Printing in upper case using .upper() method
 print()
 print(f"Upper case:\n{dc_name.upper()}")
 
+# Printing before and after stripping whitespace using repr(var) and .strip() methods
 print()
 print("Strip off whitespace:")
 print(f"Before: {repr(dc_name)}")
 print(f"After: '{dc_name.strip()}'")
 
+# Assigning modified var value after method chaining and printing its absolute value using repr(var)
 print()
 new_dc_name = dc_name.strip().upper()
 print("Method chaining:")
